44709e2da587c27816b589b0c1432a5a013275dc
sanjeevsam <47236352+sanjeevsam@users.noreply.github.com>
2019-11-19 11:53:00 +0530

    adding simple maths program

---
+++ simple_math.py
@@ -0,0 +1,18 @@
+x=10
+y=12
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**y) # exponant of number or power of number(x to the power y)
+print(x/y)  # floating point division(it give you decimal value)
+print(x//y) # interger division(it give you integer value)
+
+print(2**0.5) #rooot of num
+print(round(2**0.5,3)) #rounding of number
+#prentehes () has highest precedence
+# 2nd exponant (power of num) associativity - right to left
+# 3rd (*,/,//,%) -l to r
+#4th (+,-) -L to R
+print(7%3)
+print((5+3)*5/2%10)
+print(2**3**2)
